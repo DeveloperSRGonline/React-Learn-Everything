0dccf02a82f9d212a3878c14e949ad91b65798c2
Shivam Garade <shivamgarade05@gmail.com>
2025-10-15 21:04:31 +0530

    02 folder

---
+++ 02 components/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Shivam</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto id
+        eaque, quos doloremque veritatis in sunt placeat non ullam distinctio
+        incidunt dolores culpa a? Eius tempore facere quia corporis aut?
+      </p>
+    </div>
+  );
+};
+
+export default Card;
