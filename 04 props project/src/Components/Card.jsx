b4edba96bcc403b79af2a79ce63f2446fbfd6226
Shivam Garade <shivamgarade05@gmail.com>
2025-10-21 17:23:04 +0530

    props based project

--- 04 props project/src/Components/Card.jsx
+++ 04 props project/src/Components/Card.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Bookmark } from "lucide-react";
 
-const Card = () => {
+const Card = ({
+  company,
+  post,
+  tag1,
+  tag2,
+  brandLogo,
+  pricePerHour,
+  location,
+  datePosted,
+}) => {
   return (
-    <div className='parent'>Card</div>
-  )
-}
+    <div className="card">
+      <div>
+        <div className="top">
+          <img src={brandLogo} alt="" />
+          <button>
+            Save <Bookmark size={12} />
+          </button>
+        </div>
+        <div className="center">
+          <h3>
+            {company} <span>{datePosted}</span>
+          </h3>
+          <h2>{post}</h2>
+          <div className="tag">
+            <h4>{tag1}</h4>
+            <h4>{tag2}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div>
+          <h3>${pricePerHour}/hour</h3>
+          <p>{location}</p>
+        </div>
+        <button>Apply Now</button>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
