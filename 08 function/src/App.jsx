312f3bf6a662614a6df3a3d42b8030dd5e481d1d
Shivam Garade <shivamgarade05@gmail.com>
2025-10-21 17:25:11 +0530

    chapter 8 is in work

---
+++ 08 function/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  const btnClicked = () => {
+    console.log("button clicked");
+  }
+  
+  return (
+    <div>
+      <h1>Hello shivam</h1>
+      <button>change user</button>
+    </div>
+  )
+}
+
+export default App
